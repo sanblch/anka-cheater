c414345b0f67b2c0bdb5276f6f5dbf2ebf2db137
Alexander Krutikov <a.krutikov@innopolis.ru>
2018-09-16 16:58:09 +0300

    additional checks and output for videocut

--- misc/videocut.ps1
+++ misc/videocut.ps1
@@ -4,11 +4,30 @@ $file = $args[0]
 $t1 = $args[1]
 $t2 = $args[2]
 
+If($args.Count -eq 0) {
+    Write-Host "Program usage:" -ForegroundColor Blue
+    Write-Host "    videocut.ps1 video-filename interval1 interval2 ..." -ForegroundColor Green
+    Write-Host "    Interval - two timespans delimited with whitespace" -ForegroundColor Blue
+    Write-Host "    Format is hh:mm:ss[.f]? or mm:ss[.f]?" -ForegroundColor Blue
+    Write-Host "Examples: 01:02:03 01:02:04.5 06:07 06:08.10"
+    exit 0
+}
+
+If(-Not (Test-Path $file)) {
+    Write-Host "File $file not found!" -ForegroundColor Red
+    exit 1
+}
+
 If($args.Count % 2 -eq 0) {
     Write-Host "Check on odd number of arguments failed!" -ForegroundColor Red
     Write-Host "Not enough arguments. You've probably mistaken in time intervals." -ForegroundColor Yellow
     Write-Host "Need two numbers in each interval." -ForegroundColor Yellow
     exit 1
+}
+
+$del = "\"
+If($IsLinux) {
+    $del = "/"
 }
 
 $ts = @()
@@ -30,16 +49,18 @@ For($i = 1; $i -lt $args.Count; $i++) {
     }
 }
 
-$isMP4 = (Get-Item $file).Extension -eq ".MP4"
+$IsMP4 = (Get-Item $file).Extension -eq ".MP4"
 
+$intv = @()
 $files = @()
 For($i = 0; $i -lt $ts.Count / 2; $i++) {
     $var1 = $i * 2
     $var2 = $i * 2 + 1
     $t1 = $ts[$var1]
     $t2 = $ts[$var2] - $t1
-    $newfile = (Get-Item $file).DirectoryName + "/" + "intermediate" + $i;
-    If($isMP4) {
+    $intv += $intv[-1] + $t2
+    $newfile = (Get-Item $file).DirectoryName + $del + "intermediate" + $i;
+    If($IsMP4) {
         $newfile += ".ts"
         ffmpeg -ss $t1 -i $file -c copy -bsf:v h264_mp4toannexb -t $t2 -f mpegts $newfile -y
     } Else {
@@ -49,17 +70,33 @@ For($i = 0; $i -lt $ts.Count / 2; $i++) {
     $files += "'" + $newfile + "'"
 }
 
-$txtfile = (Get-Item $file).DirectoryName + "/" + "videolist.txt"
+$txtfile = (Get-Item $file).DirectoryName + $del + "videolist.txt"
 If(Test-Path $txtfile) {
     Remove-Item $txtfile
 }
 ForEach($f in $files) {
     $str = "file " + $f
-    Out-File -FilePath $txtfile -InputObject $str -Append
+    Out-File -FilePath $txtfile -InputObject $str -Append -Encoding ascii
 }
-$finalfile = (Get-Item $file).DirectoryName + "/" + (Get-Item $file).Basename + "-final" + (Get-Item $file).Extension
-If($isMP4) {
+$finalfile = (Get-Item $file).DirectoryName + $del + (Get-Item $file).Basename + "-final" + (Get-Item $file).Extension
+If(Test-Path $finalfile) {
+    Remove-Item $finalfile
+}
+If($IsMP4) {
     ffmpeg -safe 0 -f concat -i $txtfile -c copy -bsf:a aac_adtstoasc $finalfile -y
 } Else {
     ffmpeg -safe 0 -f concat -i $txtfile -c copy $finalfile -y
+}
+
+If(Test-Path $finalfile) {
+    Write-Host "Video concatenation successful." -ForegroundColor Blue
+    Write-Host "Don't forget to check cutting on beginning, end" -ForegroundColor Blue
+    If($ts.Count -ge 4) {
+        $str = ""
+        ForEach($i in $intv[0..($intv.Count - 2)]) {
+            $t = New-TimeSpan -Seconds $i
+            $str += " " + $t.ToString()
+        }
+        Write-Host $("and timespans:" + $str) -ForegroundColor Blue
+    }
 }
