854195daa96d5dc821d6609028d784cb2c5e69b5
Alexander Krutikov <a.krutikov@innopolis.ru>
2018-10-04 09:47:31 +0300

    extract audio script

---
+++ misc/audioextract.ps1
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/pwsh
+
+$file = $args[0]
+
+$del = "\"
+If($IsLinux) {
+    $del = "/"
+}
+
+$finalfile = (Get-Item $file).DirectoryName + $del + (Get-Item $file).Basename + ".mp3"
+
+ffmpeg -i $file -q:a 0 -map a $finalfile
