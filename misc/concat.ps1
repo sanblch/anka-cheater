36cb8c945f61bb557d533eddc7c20bb1cb7362a1
Alexander Krutikov <a.krutikov@innopolis.ru>
2018-11-06 14:33:28 +0300

    fix concat

--- misc/concat.ps1
+++ misc/concat.ps1
@@ -13,19 +13,28 @@ Param(
 )
 
 # create list of files
+$count = 0
 $list = "concat-list"
 If(Test-Path $list) {
     Remove-Item $list
 }
+$ext = (Get-Item $files[0]).Extension
 ForEach($file in $files) {
-    $str = "file " + $file
-    Out-File -FilePath $list -InputObject $str -Append -Encoding UTF8
+    $newfile = "file" + $count + $ext
+    $count++
+    Copy-Item $file -Destination $newfile
+    $str = "file " + $newfile
+    Out-File -FilePath $list -InputObject $str -Append -Encoding ascii
 }
 
 # main concatenation operation
 ffmpeg -safe 0 -f concat -i $list -c copy $outfile -y
 
 # remove temporaries
+for($item = 0; $item -lt $count; $item++) {
+    $file = "file" + $item + $ext
+    Remove-Item $file
+}
 If(Test-Path $list) {
     Remove-Item $list
 }
